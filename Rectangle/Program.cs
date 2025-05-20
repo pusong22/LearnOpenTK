d73fcdb74fc1ba24a160dbebe7ccff5ae0a0c1de
pusong <user0gao@gmail.com>
2025-05-20 17:11:45 +0800

    draw Rectangle

---
+++ Rectangle/Program.cs
@@ -0,0 +1,10 @@
+﻿using LearnOpenTK.Rectangle;
+using OpenTK.Windowing.Desktop;
+
+GameWindowSettings GameWindowSettings = GameWindowSettings.Default;
+
+NativeWindowSettings NativeWindowSettings = NativeWindowSettings.Default;
+using (var window = new RectangleWindow(GameWindowSettings, NativeWindowSettings))
+{
+    window.Run();
+}
---
+++ Rectangle/RectangleWindow.cs
@@ -0,0 +1,117 @@
+﻿using LearnOpenTK.Common;
+using OpenTK.Graphics.OpenGL4;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+#nullable disable
+namespace LearnOpenTK.Rectangle;
+
+// 使用EBO（Element Buffer Object）优化画一个矩形需要6个顶点（opengl都是画三角形），
+// EBO可以减少点数
+public class RectangleWindow(
+    GameWindowSettings gameWindowSettings,
+    NativeWindowSettings nativeWindowSettings)
+    : GameWindow(gameWindowSettings, nativeWindowSettings)
+{
+    private readonly float[] _vertices =
+    [
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left
+    ];
+
+    // _indices
+    uint[] _indices = [
+        // first triangle
+        0, 1, 3,
+        // second triangle
+        1, 2, 3
+    ];
+
+    private int _vertexBufferObject;
+    private int _vertexArrayObject;
+    private int _elementBufferObject;
+
+    private Shader _shader;
+
+    protected override void OnLoad()
+    {
+        base.OnLoad();
+
+        GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+        // copy data to buffer
+        _vertexBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+        GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+
+        _vertexArrayObject = GL.GenVertexArray();
+        GL.BindVertexArray(_vertexArrayObject);
+
+        _elementBufferObject = GL.GenBuffer();
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, _elementBufferObject);
+        GL.BufferData(BufferTarget.ElementArrayBuffer, _indices.Length * sizeof(uint), _indices, BufferUsageHint.StaticDraw);
+
+
+        GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+        GL.EnableVertexAttribArray(0);
+
+        _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+
+        _shader.Use();
+    }
+
+    protected override void OnUnload()
+    {
+        base.OnUnload();
+
+        GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+        GL.BindBuffer(BufferTarget.ElementArrayBuffer, 0);
+        GL.BindVertexArray(0);
+        GL.UseProgram(0);
+
+        GL.DeleteBuffer(_vertexBufferObject);
+        GL.DeleteBuffer(_elementBufferObject);
+        GL.DeleteVertexArray(_vertexArrayObject);
+
+        GL.DeleteProgram(_shader.Handle);
+    }
+
+    protected override void OnResize(ResizeEventArgs e)
+    {
+        base.OnResize(e);
+
+        GL.Viewport(0, 0, Size.X, Size.Y);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args)
+    {
+        base.OnRenderFrame(args);
+
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+
+        _shader.Use();
+
+        GL.BindVertexArray(_vertexArrayObject);
+
+        GL.DrawElements(PrimitiveType.Triangles, _indices.Length, DrawElementsType.UnsignedInt, 0);
+
+        SwapBuffers();
+    }
+
+    protected override void OnUpdateFrame(FrameEventArgs e)
+    {
+        base.OnUpdateFrame(e);
+
+        var input = KeyboardState;
+
+        if (input.IsKeyDown(Keys.Escape))
+        {
+            Close();
+        }
+    }
+
+}
